c39304728bb3afbc7c791519dc61f42d220042ae
asimaranov <a.simaranov@gmail.com>
2024-09-13 19:50:21 +0300

    Removed lottery

--- packages/games/config.ts
+++ packages/games/config.ts
@@ -3,7 +3,6 @@ import { numberGuessingConfig } from "./number_guessing/config";
 import { randzuConfig } from "./randzu/config";
 import { arkanoidConfig } from "./arkanoid/config";
 import { checkersConfig } from "./checkers/config";
-import { lotteryConfig } from "./lottery/config";
 import { tileVilleConfig } from "./tileville/config";
 import { pokerConfig } from "./poker/config";
 import { thimblerigConfig } from "./thimblerig/config";
@@ -12,7 +11,6 @@ import { gameTemplateConfig } from "./game-template/config";
 export const zkNoidConfig = createConfig({
   games: [
     // gameTemplateConfig,
-    lotteryConfig,
     tileVilleConfig,
     randzuConfig,
     checkersConfig,
